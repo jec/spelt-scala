380151bb142a6ff76503b9de6da10819f388b2f8
Jim Cain <jcain@spelt.io>
2025-06-23 10:37:12 -0400

    Get ask() to work [WIP]

--- src/main/scala/net/jcain/spelt/Module.scala
+++ src/main/scala/net/jcain/spelt/Module.scala
@@ -3,10 +3,16 @@ package net.jcain.spelt
 import com.google.inject.AbstractModule
 import net.jcain.spelt.service.Auth
 import net.jcain.spelt.store.{SessionStore, UserStore}
+import play.api.Logger
 import play.api.libs.concurrent.PekkoGuiceSupport
 
 class Module extends AbstractModule with PekkoGuiceSupport {
   override def configure(): Unit =
+//    val logger: Logger = Logger(this.getClass)
+
+//    logger.debug { "In configure()" }
+
+    throw new NotImplementedError("oops")
     bindTypedActor(UserStore(), "UserStoreActor")
     bindTypedActor(SessionStore(), "SessionStoreActor")
     bindTypedActor(Auth, "AuthActor")
--- src/main/scala/net/jcain/spelt/controllers/LoginController.scala
+++ src/main/scala/net/jcain/spelt/controllers/LoginController.scala
@@ -1,16 +1,26 @@
 package net.jcain.spelt.controllers
 
+import net.jcain.spelt.models.Config
 import net.jcain.spelt.service.Auth
-import org.apache.pekko.actor.typed.ActorRef
-import play.api.libs.json.{JsValue, Json}
+import org.apache.pekko.actor.typed.scaladsl.ActorContext
+import org.apache.pekko.actor.typed.scaladsl.AskPattern.Askable
+import org.apache.pekko.actor.typed.{ActorRef, ActorSystem, Scheduler}
+import org.apache.pekko.util.Timeout
+import play.api.libs.json.{JsString, JsValue, Json}
 import play.api.mvc.*
 
 import javax.inject.{Inject, Named, Singleton}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.*
+import scala.util.{Failure, Success}
 
 @Singleton
 class LoginController @Inject() (
   @Named("AuthActor") authRef: ActorRef[Auth.Request],
   val controllerComponents: ControllerComponents
+)(
+  implicit xc: ExecutionContext,
+  sch: Scheduler
 ) extends BaseController {
   /**
    * GET /_matrix/client/v3/login
@@ -32,21 +42,22 @@ class LoginController @Inject() (
    *
    * See https://spec.matrix.org/v1.14/client-server-api/#post_matrixclientv3login
    */
-  def logIn(): Action[JsValue] = Action(parse.json) { implicit request: Request[JsValue] =>
-    authRef.ask(ref => Auth.LogIn(request.body.asJson.get, ref)) {
-      case Success(Auth.LoginSucceeded(identifier, token, deviceId)) =>
-        Ok(Json.obj(
-          "access_token" -> token,
-          "device_id" -> deviceId,
-          "user_id" -> identifier,
-          "well_known" -> Config.wellKnown
-        ))
+  def logIn(): Action[JsValue] = Action.async(parse.json) { request =>
+    implicit val timeout: Timeout = 5.seconds
+    val body = request.body
 
-      case Success(Auth.LoginFailed(message)) =>
-        Unauthorized(Json.obj("error_message" -> message))
+    authRef.ask(ref => Auth.LogIn(body, ref))
+      .map {
+        case Auth.LoginSucceeded(identifier, token, deviceId) =>
+          Ok(Json.obj(
+            "access_token" -> JsString(token),
+            "device_id" -> JsString(deviceId),
+            "user_id" -> JsString(identifier),
+            "well_known" -> Config.wellKnown
+          ))
 
-      case Failure(error) =>
-        InternalError(Json.obj("error_message" -> message))
-    }
+        case Auth.LoginFailed(message) =>
+          Unauthorized(Json.obj("error_message" -> JsString(message)))
+      }
   }
 }
